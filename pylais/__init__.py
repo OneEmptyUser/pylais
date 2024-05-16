7a3a23fb7d2947f0bc481998ee03af2ba92c1a38
Ernesto Curbelo <eacb1996@gmail.com>
2024-05-16 15:12:24 +0200

    fix errors importing

---
+++ pylais/__init__.py
@@ -0,0 +1,1 @@
+from .lais import Lais
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="pylais",
     version="0.0.1",
-    packages=["pylais", "tensorflow", "tfprobability"],
+    packages=["pylais"],
     author="OneEmptyUser",
     author_email="pDd5A@example.com",
 )
