dde8cfa2adc614eeecec1b0d8c704c3216fb28c8
Ernesto Curbelo <eacb1996@gmail.com>
2024-08-03 13:21:36 +0200

    add student-t proposal to calculate denominators

--- pylais/denominators.py
+++ pylais/denominators.py
@@ -3,100 +3,143 @@ import tensorflow as tf
 import tensorflow_probability as tfp
 mvn = tfp.distributions.MultivariateNormalTriL
 
-def spatial(means, samples, cov):
+def spatial(means, samples, proposal_settings):
     """
     Calculate the spatial denominator for each sample.
-    
+
     Calculate the spatial denominator as the average of the density evaluation
     on each sample of all the proposals that were adapted at the same time
     that the proposal that originated the sample.
 
     Parameters
     ----------
-        means: tensorflow.Tensor
-            Tensor of shape (N, T, dim) representing the means.
-        samples: tensorflow.Tensor
-            Tensor of shape (_, n_samples, _) representing the samples.
-        cov_down: tensorflow.Tensor
-            The covariance matrix of the proposals
+    means : tensorflow.Tensor
+        Tensor of shape (N, T, dim) representing the means.
+    samples : tensorflow.Tensor
+        Tensor of shape (_, n_samples, _) representing the samples.
+    proposal_settings : dict
+        Dictionary containing the proposal settings, including the covariance matrix and the proposal type.
 
     Returns
     -------
-        dens: tensorflow.Tensor
-            Tensor of shape (N, n_samples, 1) representing the spatial denominator.
+    dens : tensorflow.Tensor
+        Tensor of shape (n_samples,) representing the spatial denominator.
     """
     
     N, T, dim = means.shape
     _, n_samples, _ = samples.shape
+    
+    cov = proposal_settings.get("cov", tf.eye(dim))
+    scale = tf.linalg.cholesky(cov)
+    if proposal_settings.get("proposal_type", "gaussian") == "gaussian":
+        proposal = tfp.distributions.MultivariateNormalTriL(loc=tf.zeros(dim, dtype=tf.float64),
+                                                            scale_tril=scale)
+    
+    if proposal_settings.get("proposal_type", "gaussian") == "student":
+        df = proposal_settings.get("df", dim + 1)
+        proposal = tfp.distributions.MultivariateStudentTLinearOperator(df,
+                                                                        loc=tf.zeros(dim, dtype=tf.float64),
+                                                                        scale=tf.linalg.LinearOperatorLowerTriangular(scale))
+        
     dens = []
     k = n_samples // T # number of samples per proposal
     
     for n in range(N):
         for t in range(n_samples):
+            loc = samples[n, t, :]
             dens.append(
-                tf.math.reduce_mean(mvn(loc=samples[n, t, :], scale_tril=tf.linalg.cholesky(cov)).prob(means[:, t//k, :]))
+                tf.math.reduce_mean(proposal.prob(means[:, t//k, :] - loc))
                 )
     dens = tf.stack(dens) 
     return dens
 
-def temporal(means, samples, cov):
+def temporal(means, samples, proposal_settings):
     """
     Calculate the temporal denominator for each sample.
-    
+
     Calculate the temporal denominator as the average of the density evaluation
-    on each sample of all the proposals that were adapted in the same chain as
-    the proposal that originated the sample.
+    on each sample of all the proposals that were adapted at the same time
+    that the proposal that originated the sample.
 
     Parameters
     ----------
-        means: tensorflow.Tensor
-            Tensor of shape (N, T, dim) representing the means.
-        samples: tensorflow.Tensor
-            Tensor of shape (_, n_samples, _) representing the samples.
-        cov: tensorflow.Tensor
-            The covariance matrix of the proposals
+    means : tensorflow.Tensor
+        Tensor of shape (N, T, dim) representing the means.
+    samples : tensorflow.Tensor
+        Tensor of shape (_, n_samples, _) representing the samples.
+    proposal_settings : dict
+        Dictionary containing the proposal settings, including the covariance matrix and the proposal type.
 
     Returns
     -------
-        dens: tensorflow.Tensor
-            Tensor of shape (N, n_samples, 1) representing the temporal denominator
+    dens : tensorflow.Tensor
+        Tensor of shape (n_samples,) representing the temporal denominator.
     """
+    
     N, T, dim = means.shape
     _, n_samples, _ = samples.shape
+    
+    cov = proposal_settings.get("cov", tf.eye(dim))
+    scale = tf.linalg.cholesky(cov)
+    if proposal_settings.get("proposal_type", "gaussian") == "gaussian":
+        proposal = tfp.distributions.MultivariateNormalTriL(loc=tf.zeros(dim, dtype=tf.float64),
+                                                            scale_tril=scale)
+    
+    if proposal_settings.get("proposal_type", "gaussian") == "student":
+        df = proposal_settings.get("df", dim + 1)
+        proposal = tfp.distributions.MultivariateStudentTLinearOperator(df,
+                                                                        loc=tf.zeros(dim, dtype=tf.float64),
+                                                                        scale=tf.linalg.LinearOperatorLowerTriangular(scale))
     
     dens = []
     for n in range(N):
         for t in range(n_samples):
+            loc = samples[n, t, :]
             dens.append(
-                tf.math.reduce_mean(mvn(loc=samples[n, t, :], scale_tril=tf.linalg.cholesky(cov)).prob(means[n, :, :]))
+                tf.math.reduce_mean(proposal.prob(means[n, :, :] - loc))
                 )
             
     dens = tf.stack(dens)
     return dens
 
-def all_(flatted_means, flatted_samples, cov):
+def all_(flatted_means, flatted_samples, proposal_settings):
     """
     Calculate the total denominator for each sample.
-    
+
     Calculate the total denominator as the average of the density evaluation
     of all the proposals on each sample.
-    
+
     Parameters
     ----------
-        flatted_means: tensorflow.Tensor
-            The tensor of means with shape (N, T, dim).
-        flatted_samples: tensorflow.Tensor
-            The tensor of samples with shape (_, n_samples, _).
-        cov: tensorflow.Tensor
-            The covariance matrix of the proposals with shape (dim, dim).
+    flatted_means : tensorflow.Tensor
+        The tensor of means with shape (N*T, dim).
+    flatted_samples : tensorflow.Tensor
+        The tensor of samples with shape (n_samples, _).
+    proposal_settings : dict
+        Dictionary containing the proposal settings, including the covariance matrix and the proposal type.
 
     Returns
     -------
-        dens: tensorflow.Tensor
-            The tensor of denominators with shape (N, n_samples, 1).
+    dens : tensorflow.Tensor
+        The tensor of denominators with shape (n_samples,).
     """
+    
+    
+    _, dim = flatted_means.shape
+    cov = proposal_settings.get("cov", tf.eye(dim))
+    scale = tf.linalg.cholesky(cov)
+    if proposal_settings.get("proposal_type", "gaussian") == "gaussian":
+        proposal = tfp.distributions.MultivariateNormalTriL(loc=tf.zeros(dim, dtype=tf.float64),
+                                                            scale_tril=scale)
+    
+    if proposal_settings.get("proposal_type", "gaussian") == "student":
+        df = proposal_settings.get("df", dim + 1)
+        proposal = tfp.distributions.MultivariateStudentTLinearOperator(df,
+                                                                        loc=tf.zeros(dim, dtype=tf.float64),
+                                                                        scale=tf.linalg.LinearOperatorLowerTriangular(scale))
+    
     aux_fn = tf.function(
-        lambda x: tf.math.reduce_mean(mvn(loc=x, scale_tril=tf.linalg.cholesky(cov)).prob(flatted_means))
+        lambda x: tf.math.reduce_mean(proposal.prob(flatted_means - x))
         )
     
     dens = tf.map_fn(
